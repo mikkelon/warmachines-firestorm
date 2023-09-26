7284746e5ecbd929fc64694157e347df5a5bd3a5
mikkelon <mikkelovergaardnielsen@gmail.com>
2023-09-26 11:15:22 +0200

    initial commit

---
+++ src/game/App.java
@@ -0,0 +1,16 @@
+package game;
+
+import java.net.*;
+import java.io.*;
+import javafx.application.Application;
+
+public class App {
+	public static void main(String[] args) throws Exception{
+		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Indtast spillernavn");
+		String navn = inFromUser.readLine();
+		GameLogic.makePlayers(navn);
+		Application.launch(Gui.class);
+	}
+}
+;
