5ce6df1db7002819527d61bec81d1a41f52aecee
mikkelon <mikkelovergaardnielsen@gmail.com>
2023-09-26 11:59:45 +0200

    import new assets

--- src/game/Gui.java
+++ src/game/Gui.java
@@ -60,13 +60,13 @@ public class Gui extends Application {
 			
 			GridPane boardGrid = new GridPane();
 
-			image_wall  = new Image(getClass().getResourceAsStream("Image/wall4.png"),size,size,false,false);
-			image_floor = new Image(getClass().getResourceAsStream("Image/floor1.png"),size,size,false,false);
+			image_wall  = new Image(getClass().getResourceAsStream("assets/wall-brick2.png"),size,size,false,false);
+			image_floor = new Image(getClass().getResourceAsStream("assets/floor.png"),size,size,false,false);
 
-			hero_right  = new Image(getClass().getResourceAsStream("Image/heroRight.png"),size,size,false,false);
-			hero_left   = new Image(getClass().getResourceAsStream("Image/heroLeft.png"),size,size,false,false);
-			hero_up     = new Image(getClass().getResourceAsStream("Image/heroUp.png"),size,size,false,false);
-			hero_down   = new Image(getClass().getResourceAsStream("Image/heroDown.png"),size,size,false,false);
+			hero_right  = new Image(getClass().getResourceAsStream("assets/tank1-right.png"),size,size,false,false);
+			hero_left   = new Image(getClass().getResourceAsStream("assets/tank1-left.png"),size,size,false,false);
+			hero_up     = new Image(getClass().getResourceAsStream("assets/tank1-up.png"),size,size,false,false);
+			hero_down   = new Image(getClass().getResourceAsStream("assets/tank1-down.png"),size,size,false,false);
 
 			fields = new Label[20][20];
 			for (int j=0; j<20; j++) {
