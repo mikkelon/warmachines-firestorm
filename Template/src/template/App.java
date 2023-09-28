68862f15d311de5bbe1842b2ad99a414dc6511af
Emil Egelund <emilmondrupjacobsen@gmail.com>
2023-09-28 11:54:25 +0200

    GameClient implementation in progress

--- Template/src/template/App.java
+++ Template/src/template/App.java
@@ -1,7 +1,9 @@
 package template;
 
-import java.io.*;
 import javafx.application.Application;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class App {
 	public static void main(String[] args) throws Exception{
--- src/server/GameServer.java
+++ src/server/GameServer.java
@@ -9,6 +9,7 @@ public class GameServer {
         ServerSocket welcomeSocket = new ServerSocket(6789);
         while (true) {
             Socket connectionSocket = welcomeSocket.accept();
+            System.out.println("New connection");
             (new PlayerThread(connectionSocket)).start();
         }
     }
